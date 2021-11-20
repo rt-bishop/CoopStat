65057796919e98043dc2fe68a4790ad1b7aed0b2
Arty Bishop <bishop.arty@gmail.com>
2021-11-20 12:20:16 +0000

    Updated dependencies, code cleanup

--- app/src/main/java/priv/rtbishop/coopstat/MainActivity.kt
+++ app/src/main/java/priv/rtbishop/coopstat/MainActivity.kt
@@ -2,24 +2,23 @@ package priv.rtbishop.coopstat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import priv.rtbishop.coopstat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ActivityMainBinding.inflate(layoutInflater).apply {
-            setContentView(root)
-            val proxyUrl = "http://192.168.192.200:8554/?action=stream"
-            streamView.viewTreeObserver.apply {
-                if (this.isAlive) {
-                    addOnGlobalLayoutListener {
-                        val scale = (streamView.width / 2592f * 100).toInt()
-                        streamView.apply {
-                            setInitialScale(scale)
-                            loadUrl(proxyUrl)
-                        }
-                    }
+        installSplashScreen()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val proxyUrl = "http://192.168.192.200:8554/?action=stream"
+        binding.streamView.viewTreeObserver.apply {
+            if (isAlive) {
+                addOnGlobalLayoutListener {
+                    val scale = (binding.streamView.width / 2592f * 100).toInt()
+                    binding.streamView.setInitialScale(scale)
+                    binding.streamView.loadUrl(proxyUrl)
                 }
             }
         }
